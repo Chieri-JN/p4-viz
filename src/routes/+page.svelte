158f39373387d2d5eab5989bbcabacb20e0ddd83
Chieri-JN <115610242+Chieri-JN@users.noreply.github.com>
2026-04-21 12:47:55 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import * as d3 from "d3";
+</script>
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
+<style>
+    
+</style>
